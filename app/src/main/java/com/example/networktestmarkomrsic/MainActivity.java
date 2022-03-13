99a0694c7d935607897a7634e500d8c5d6a40934
MrsicMarko <marko2605@gmx.at>
2022-03-13 17:29:45 +0100

    Aufgabe 2.2 Berechnung hinzugefügt

--- app/src/main/java/com/example/networktestmarkomrsic/MainActivity.java
+++ app/src/main/java/com/example/networktestmarkomrsic/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewMatNr;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView serverAnswer;
     private EditText inputMatNr;
     private Button buttonSend;
+    private Button calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         inputMatNr = (EditText) findViewById(R.id.inputMatNr);
         buttonSend = (Button) findViewById(R.id.buttonSend);
         serverAnswer = (TextView) findViewById(R.id.serverAnswer);
+        calculate = (Button) findViewById(R.id.calc);
 
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +45,39 @@ public class MainActivity extends AppCompatActivity {
                 serverAnswer.setText(t.get());
             }
         });
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String matNrString = inputMatNr.getText().toString();
+                char[] cArr = matNrString.toCharArray();
+
+                Random random = new Random();
+                int n = random.nextInt(8);
+                int zwischen = random.nextInt(8);
+                while(zwischen == n){
+                    zwischen = random.nextInt(8);
+                }
+                int m = zwischen;
+                int zahl1 = matNrString.charAt(n) - '0';
+                int zahl2 = matNrString.charAt(m) - '0';
+                if(ggt(zahl1, zahl2) > 1){
+                    serverAnswer.setText(m+" "+n);
+                }else{
+                    serverAnswer.setText("Kein gemeinsamer Teiler >1 gefunden");
+                }
+            }
+        });
+    }
+    public int ggt(int a, int b) {
+
+        if (a == 0)
+            return b;
+        while (b != 0) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        return a;
     }
 }
