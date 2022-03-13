04cb6176202e38b10f7a5e357f7200fd245bcca9
MrsicMarko <marko2605@gmx.at>
2022-03-13 17:13:04 +0100

    App design, Aufgabe 2.1

--- app/src/main/java/com/example/networktestmarkomrsic/MainActivity.java
+++ app/src/main/java/com/example/networktestmarkomrsic/MainActivity.java
@@ -3,14 +3,36 @@ package com.example.networktestmarkomrsic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMatNr;
+    private TextView textViewServer;
+    private TextView serverAnswer;
+    private EditText inputMatNr;
+    private Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewMatNr = (TextView) findViewById(R.id.textViewMatNr);
+        textViewServer = (TextView) findViewById(R.id.textViewServer);
+        inputMatNr = (EditText) findViewById(R.id.inputMatNr);
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+        serverAnswer = (TextView) findViewById(R.id.serverAnswer);
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 }
